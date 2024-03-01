3ede38d535b524ef6f708fc6f4d37f330020c06b
panchi <pancho@gmail.com>
2024-02-29 22:31:17 -0300

    first commit

---
+++ lib/handler.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'package:fluro/fluro.dart';
+import 'package:flurox/main.dart';
+
+class MyRouter {
+  
+ static FluroRouter router = FluroRouter();
+
+
+static void configuracionRouter(){
+
+  router.define("page1", handler: Handler(handlerFunc: (context, parameters) => PageUno(),));
+  router.define("page2", handler: Handler(handlerFunc: (context, parameters) => PageDos(),));
+    router.define("home", handler: Handler(handlerFunc: (context, parameters) => MyBody(),));
+
+  
+}
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:fluro/fluro.dart';
+import 'package:flurox/handler.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  void initState() {
+    MyRouter.configuracionRouter();
+    super.initState();
+  }
+  
+  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fluro',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: const MyHomePage(),
+      initialRoute: "home",
+      onGenerateRoute: MyRouter.router.generator,
+    );
+  }
+}
+
+class MyBody extends StatelessWidget {
+  const MyBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(onPressed: (){MyRouter.router.navigateTo(context, "page1");}, child:const  Text("page 1")),
+          TextButton(onPressed: (){MyRouter.router.navigateTo(context, "page2");}, child:const  Text("page 2")),
+    
+        ],
+      ),
+    );
+  }
+}
+
+
+class PageUno extends StatelessWidget {
+  const PageUno({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
+
+class PageDos extends StatelessWidget {
+  const PageDos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
