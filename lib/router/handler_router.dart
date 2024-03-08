7b37fa5d1fa73a9b986dabddc0e8277aeede3fae
pancho <pancho@pancho.com>
2024-03-08 00:09:13 -0300

    x

--- lib/router/handler_router.dart
+++ lib/router/handler_router.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:fluro/fluro.dart';
 import 'package:flurox/Layout/Page_home.dart';
 
@@ -10,37 +7,44 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MyRouter {
-  
- static FluroRouter router = FluroRouter();
+  static FluroRouter router = FluroRouter();
 
+  static void configuracionRouter() {
+    router.define("/:page1", handler: Handler(
+      handlerFunc: (context, parameters) {
+        print("parametros page1$parameters");
+        return Home(child: MyCustomPage());
+      },
+    ));
 
-static void configuracionRouter(){
+    router.define("/:page2", handler: Handler(
+      handlerFunc: (context, parameters) {
+        print("parametros page2$parameters");
 
-  router.define("/page1", handler: Handler(handlerFunc: (context, parameters) {
-    print("parametros page1$parameters");
-    return  Home(child: MyCustomPage()) ;},));
+        return Home(child: MyCustomPage());
+      },
+    ));
 
-     router.define("/page2", handler: Handler(handlerFunc: (context, parameters) {
-     print("parametros page2$parameters");
-      final provider=Provider.of<MyCustomProvider>(context!,listen: false);
-      provider.pagex=2;
-     return  Home(child: MyCustomPage()) ;},));
+    router.define("/:page3", handler: Handler(
+      handlerFunc: (context, parameters) {
+        print("parametros page3$parameters");
+        final provider = Provider.of<MyCustomProvider>(context!, listen: false);
+        provider.pagex = 3;
+        return Home(child: MyCustomPage());
+      },
+    ));
 
-     router.define("/page3", handler: Handler(handlerFunc: (context, parameters) {
-     print("parametros page3$parameters");
-     final provider = Provider.of<MyCustomProvider>(context!,listen: false);
-     provider.pagex=3;
-     return  Home(child: MyCustomPage()) ;},));
+    router.define("/:page4", handler: Handler(
+      handlerFunc: (context, parameters) {
+        print("parametros page4$parameters");
+        return Home(child: MyCustomPage());
+      },
+    ));
 
-     router.define("/page4", handler: Handler(handlerFunc: (context, parameters) {
-     print("parametros page4$parameters");
-     return  Home(child: MyCustomPage()) ;},));
-  
-    
-    router.notFoundHandler=Handler(handlerFunc: (context, parameters) => Center(child: const Text("404"),),);
-  
-}
-
-
-
+    router.notFoundHandler = Handler(
+      handlerFunc: (context, parameters) => Center(
+        child: const Text("404"),
+      ),
+    );
+  }
 }
